2e7809b2e66074bb52fc11af736c09f848f248af
devlucasrocha <lucasgeorgerocha@gmail.com>
2019-05-08 16:14:26 -0300

    Sistema de Módulos

---
+++ moduloCliente.js
@@ -0,0 +1,13 @@
+const moduloA = require('./moduloA')
+const moduloB = require('./moduloB')
+
+console.log("Modulo A: \n")
+console.log(moduloA.ola)
+console.log(moduloA.bemVindo)
+console.log(moduloA.ateLogo)
+console.log(moduloA)
+
+console.log("\nModulo B: \n")
+console.log(moduloB.bomDia)
+console.log(moduloB.boaNoite())
+console.log(moduloB)
